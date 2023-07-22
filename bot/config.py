993c66a8b23f5896f25c9af97065694c2a0e56ec
KNNL19 <137700034+KNNL19@users.noreply.github.com>
2023-07-22 15:38:06 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,10 +3,10 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+BOT_TOKEN = "5840948978:AAEkP_a_WojuNmmUq5jy_BXtarORCozjThE"
+GDRIVE_FOLDER_ID = "1GTt3P72lqHATeJdjLYwvFPrJ9EW1sq0t"
 # Default folder id.
-OWNER_ID = 123455673
+OWNER_ID = 1611819637
 # Example: OWNER_ID = 619418070
 AUTHORISED_USERS = []
 # Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
